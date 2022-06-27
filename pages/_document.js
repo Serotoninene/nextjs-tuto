c1ce9494d198b5fbc7d31b5749a4eb5a61a5365b
Serotoninene <pujol.alexandre@hotmail.fr>
2022-06-27 17:16:07 +0200

    learning nextjs

---
+++ pages/_document.js
@@ -0,0 +1,16 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+// Components
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta description="Les proutss" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
